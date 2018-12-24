e8d42639c43fe8ca5bdc94d3417543e1d0dd993c
Sebastien DIAZ <sebastien.diaz@gmail.com>
2018-12-24 17:09:22 +0100

    contact

--- src/containers/company/Contact.tsx
+++ src/containers/company/Contact.tsx
@@ -1,9 +1,44 @@
 import React from "react";
 
 const Research = () => (
-  <div>
-    <h2>About Page</h2>
-  </div>
+  <div className="container marketing">
+  
+  
+          <hr className="featurette-divider"/>
+  
+          <div className="row featurette">
+            <div className="col-md-7">
+              <h2 className="featurette-heading">Contact</h2>
+              <h3>Buisson Diaz Conseil.</h3>
+              <p className="lead"><b>Paris Head Office.</b></p>
+
+              <p className="lead">8 Allée Joseph Récamier</p>
+
+              <p className="lead">75015 Paris - France</p>
+
+              <p className="lead"></p>
+
+              <p className="lead">Phone: +33 6 15 53 07 20</p>
+
+              <p className="lead">Email : sebastien.diaz[at]buissondiaz.com</p>
+            </div>
+            <div className="col-md-5">
+                    <iframe width="400" height="350" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="http://cartosm.eu/map?lon=2.2970654902108&lat=48.835200330486&zoom=18&width=400&height=350&mark=true&nav=false&pan=true&zb=inout&style=default&icon=down"></iframe>
+                    <br />
+                    <div id="cartosmlink">
+                    <a href="http://www.openstreetmap.org/?mlat=48.835200330486&mlon=2.2970654902108&zoom=18&layers=M" >See a better Map</a>
+                    </div>
+
+            </div>
+          </div>
+  
+          
+  
+          <hr className="featurette-divider"/>
+  
+          
+  
+        </div>
 );
 
 export default Research;
