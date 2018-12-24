dd6b45acc06088e0ef522216c04667ee977ed174
Sebastien DIAZ <sebastien.diaz@gmail.com>
2018-12-24 17:21:02 +0100

    analytics

--- src/containers/data/Analytics.tsx
+++ src/containers/data/Analytics.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 
 const Development = () => (
-  <div>
-    <h2>About Page</h2>
-  </div>
+  <div >
+  <hr />
+  <div className="row">
+  <blockquote class="blockquote">
+              <h2 >Deep Data Analitycs.</h2>
+              <p className="lead text-left">
+                We are expert to sample, transform, evaluate and analyse your data. 
+                We estimate the multifactorial densities of your systems. 
+                We search for patterns, segments or clusters. 
+                We make complex sampling and straightening. 
+                We manage the design of your experiments, the quality of the processus, the performance analisys, customers satisfaction, service quality evaluation, Six Sigma and Lean Solutions . 
+                We make factorial, hierarchical, mixed and optimal experiments plan. 
+                We made Correspondence and Components Analisys on any kind of data. 
+                We study saisonality and tendancy of your TimeSeries. we make complex sampling of your data including strata and quotas. 
+                We certify your data effect by parametric and non parametric tests. 
+                We analyse your geostatistical data to determine effects and correlations.
+                We make the automation of your data analyse and results on any platform or statistical software. 
+                We optimize the data transformation and calculation for memory and time reduction. 
+                We make Massive and Parallel computations.</p>
+              <img className="featurette-image img-fluid mx-auto" src="http://www.thebluediamondgallery.com/handwriting/images/data.jpg" alt="Generic placeholder image" />
+            </blockquote>
+          </div>
+          </div>
 );
 
 export default Development;
