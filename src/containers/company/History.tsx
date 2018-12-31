a788e2f1d318813ab42fa09cb7b97372b9279fdf
Sebastien DIAZ <sebastien.diaz@gmail.com>
2018-12-31 08:05:49 +0100

    New content

--- src/containers/company/History.tsx
+++ src/containers/company/History.tsx
@@ -2,7 +2,19 @@ import React from "react";
 
 const Development = () => (
   <div>
-    <h2>About Page</h2>
+    <hr />
+    <h1>Since 2018,</h1>
+    <p>Buisson Diaz Conseil is a Software Services Company spéecialized in Data Management and Software Development.</p>
+    <p>Buisson Diaz Conseil supports you to deliver the advice and all the expertise expected by customers who are always taking more initiatives driven by new developments such as cloud, big data or mobility..</p>
+    <p>Buisson Diaz Conseil  has been growing steadily since the beginning of its existence.</p>
+    <p>
+    This creation was motivated by two elements:
+    <ul>
+<li>The search by a large account of a company with specific skills for the creation of package software with a commitment to the result.</li>
+<li>The demand for assistance from many SMEs without IT services. In fact, all the same, they must acquire and manage the same services as those of a large company.</li>
+</ul>
+</p>
+    <p>Our team is at your disposal to provide you with all the necessary details on our solutions.</p>
   </div>
 );
 
--- src/containers/company/Team.tsx
+++ src/containers/company/Team.tsx
@@ -2,7 +2,18 @@ import React from "react";
 
 const Development = () => (
   <div>
-    <h2>About Page</h2>
+    <hr/>
+    <h2>Team</h2>
+    <p>
+      <h3>Sébastien DIAZ</h3>
+      <h4>CEO</h4>
+<p>
+
+He most recently served as Chairman and CEO of Formation Data Services, developers of a revolutionary cloud software storage platform for replace traditional arrays. He previously worked in various executive capacities at EMC, where he was instrumental in helping transform the business from a single-product hardware company to a leading IT solutions provider. Before EMC, Mark was Vice President and General Manager of Hewlett-Packard/Compaq/Digital’s $2.5 billion Enterprise Storage Group, which was the largest storage organization in the world at the time, and served in various storage-related business, engineering and product development roles at Digital Equipment Corporation.
+
+He is currently a board member at Ciber Corporation, a leading provider of IT services. Mark has been granted more than 10 U.S. patents for hardware and software technology. He graduated from the University of Colorado, Boulder in 1984 with a B.S. in Mechanical Engineering.
+</p>
+    </p>
   </div>
 );
 
