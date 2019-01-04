a48dc41ad267f3ff0ff6bc55c600799a01c111ca
Sebastien DIAZ <sebastien.diaz@gmail.com>
2019-01-04 11:18:34 +0100

    toute

--- pyt/second.py
+++ pyt/second.py
@@ -22,49 +22,53 @@ for root, dirs, files in os.walk("."):
             except:
                 print ('clean '+filecsv)
             listInterval =[]
-            with open(filename, newline='') as csvfile:
-                spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
-                lot=0
-                maxRow=20
-                listRow = []
+
+            for maxRow in [20,60,300]:
                 
-                for row in spamreader:
-                    if(lot>maxRow):
-                        timeMin=0
-                        timeMax=0
-                        
-                        for rowListmim in listRow:
-                             interval=0
-                             for rowListmax in listRow:
-                                if float(rowListmax[2])-float(rowListmim[2])>interval:
-                                    timeMin=rowListmim[0]
-                                    timeMax=rowListmax[0]
-                                    interval=float(rowListmax[2])-float(rowListmim[2])
-                                    listInterval.append({'interval':interval,'timeMin':timeMin,'timeMax':timeMax,'window':maxRow})
-                        listRow.pop(0)
-                                
-                        lot=lot=lot-1
-                    else:
-                        listRow.append(row)
-                        lot=lot+1
-            finalList=[]
-            setTime=set()
-            for row in listInterval:
-                 
-                timeref=row['timeMin']
-                if timeref in setTime:
-                    continue
-                setTime.add(timeref)
-                best=row
-                for rowSec in listInterval:
-                    if timeref==rowSec['timeMin'] and rowSec['interval']>row['interval']:
-                        best=rowSec
-                finalList.append(best)
-            finalList=sorted(finalList, key=lambda student: float(student['interval']))
-            with open(filecsv, 'a', newline='') as csvfile:
-                spamwriter = csv.writer(csvfile, delimiter=';',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                for row in finalList:
-                    spamwriter.writerow([row['interval'],row['timeMin'],row['timeMax'],row['window']])
+                with open(filename, newline='') as csvfile:
+                    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+                    lot=0
+                    
+                    listRow = []
+                    
+                    for row in spamreader:
+                        if(lot>maxRow):
+                            timeMin=0
+                            timeMax=0
+                            
+                            for rowListmim in listRow:
+                                interval=0
+                                for rowListmax in listRow:
+                                    newInterval=(float(rowListmax[2])-float(rowListmim[2]))/float(rowListmim[2])
+                                    if newInterval>interval:
+                                        timeMin=rowListmim[0]
+                                        timeMax=rowListmax[0]
+                                        interval=newInterval
+                                        listInterval.append({'interval':interval,'timeMin':timeMin,'timeMax':timeMax,'window':maxRow})
+                            listRow.pop(0)
+                                    
+                            lot=lot=lot-1
+                        else:
+                            listRow.append(row)
+                            lot=lot+1
+                finalList=[]
+                setTime=set()
+                for row in listInterval:
+                    
+                    timeref=row['timeMin']
+                    if timeref in setTime:
+                        continue
+                    setTime.add(timeref)
+                    best=row
+                    for rowSec in listInterval:
+                        if timeref==rowSec['timeMin'] and rowSec['interval']>row['interval']:
+                            best=rowSec
+                    finalList.append(best)
+                finalList=sorted(finalList, key=lambda student: float(student['interval']))
+                with open(filecsv, 'a', newline='') as csvfile:
+                    spamwriter = csv.writer(csvfile, delimiter=';',
+                                        quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    for row in finalList:
+                        spamwriter.writerow([row['interval'],row['timeMin'],row['timeMax'],row['window']])
             print('write '+filecsv)
 
