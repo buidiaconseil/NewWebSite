11f3848187e7daaccb35aa842f5af01b4b69b1b5
SEBASTIEN DIAZ <sebastien-externe.diaz@enedis.fr>
2019-02-07 14:29:04 +0100

    push learning

--- src/containers/solutions/Trading.tsx
+++ src/containers/solutions/Trading.tsx
@@ -1,26 +1,60 @@
 import React from "react";
-import {encode, decode} from "messagepack";
+import * as tf from '@tensorflow/tfjs';
 const axios = require('axios');
+import * as Papa from 'papaparse';
+import { RandomForestClassifier } from 'machinelearn/ensemble';
+import { SGDClassifier } from 'machinelearn/linear_model';
+import { KNeighborsClassifier } from 'machinelearn/neighbors';
 
-
-
-axios.get('indicators-ETH-BTC.mpk',{
-                                                                                                           headers: {
-                                                                                                                responseType: 'arraybuffer'
-                                                                                                           }
-                                                                                                       })
+axios.get('indicators-ETH-BTC.csv')
   .then(function (response) {
     // handle success
     console.log(response);
     console.log(typeof(response.data));
-    var buf = new ArrayBuffer(response.data.length*2); // 2 bytes for each char
-      var bufView = new Uint8Array(buf);
-      for (var i=0, strLen=response.data.length; i < strLen; i++) {
-        bufView[i] = response.data.charCodeAt(i);
+    var data = Papa.parse(response.data,{
+                                       	dynamicTyping: true
+                                       });
+    var x=[];
+    var yLong=[];
+    var yShort=[];
+    var i;
+    console.log(data.data.length);
+    for (i = 0; i < data.data.length; i++) {
+      var col=data.data[i];
+      if(col[0]!=null){
+      yLong.push(col[0]);
+      yShort.push(col[1]);
+      var j;
+      var dataLine=[];
+          for (j = 2; j < col.length; j++) {
+               dataLine.push(col[j]);
+          }
+
+      x.push(dataLine);
       }
-      console.log(buf);
-    const decodedData = decode(buf);
-    console.log(decodedData);
+    }
+    console.log(x.length);
+    console.log(yShort);
+    console.log(yLong);
+    console.log(x);
+    const randomForest = new RandomForestClassifier();
+    randomForest.fit(x, yLong);
+    console.log(randomForest.toJSON());
+    const clf = new SGDClassifier();
+    clf.fit(x, yLong);
+    console.log(clf.toJSON());
+    const knn = new KNeighborsClassifier();
+    knn.fit(x, yLong);
+    console.log(knn.toJSON());
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape: [151]}));
+    model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+    const xs = tf.tensor2d(x, [29,151]);
+    const ys = tf.tensor2d(yLong, [29,1]);
+    model.fit(xs, ys, {epochs: 10}).then(() => {
+        console.log(JSON.stringify(model.outputs[0].shape));
+    });
+
   })
 
 
